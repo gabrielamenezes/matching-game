ccf15d350068ffea2e23d97bbd6285a71fb97e77
Gabriela Menezes <64161478+gabrielamenezes@users.noreply.github.com>
2022-06-11 19:28:12 -0300

    Add html comment

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -1,7 +1,7 @@
 import './style.css'
 // JSX
 function CardGame() {
-    return `
+    return /*html*/ `
         <article class="card-game">
             <img src="images/alura-pixel.png" alt="Logo da Alura">
         </article>
