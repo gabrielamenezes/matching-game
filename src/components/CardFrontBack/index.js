a993c95e8c25d1f7b053a2bc6099117db4f4f100
Gabriela Menezes <64161478+gabrielamenezes@users.noreply.github.com>
2022-06-11 21:07:13 -0300

    Add flip CardGame

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,10 +1,16 @@
 import CardGame from "../CardGame";
-
+import './style.css';
 function CardFrontBack() {
     return /*html*/ `
         <article class="card-front-back">
-        ${CardGame()}
-        ${CardGame("javascript", "Logo do Javascript")}
+        <div class="card -front">
+            ${CardGame()}
+        </div>
+        
+        <div class="card -back">
+            ${CardGame("javascript", "Logo do Javascript")}
+        </div>
+        
         </article>
     `
 }
